3eb935c1ff5907915c380a21708eddfb594b5da1
shivamgcs9 <shivamgcs9@gmail.com>
2024-01-21 12:50:57 +0530

    added about and contact page UI

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,12 @@
+import styles from "./footer.module.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Logo</div>
+      <div className={styles.text}>
+        Logo creative thoughts agency @ all rights reserved.
+      </div>
+    </div>
+  );
 };
 export default Footer;
