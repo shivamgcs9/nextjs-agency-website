2e4dbd6d33de43dab78b6e37fb992cdfd454a386
shivamgcs9 <shivamgcs9@gmail.com>
2024-01-28 14:18:40 +0530

    added blog page and single blog page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,46 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+
 const SinglePostPage = () => {
-  return <div>SinglePostPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/845405/pexels-photo-845405.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            src="https://images.pexels.com/photos/845405/pexels-photo-845405.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+            width={50}
+            height={50}
+            className={styles.avatar}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Shivam</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+          nam quis animi enim laboriosam, eveniet ducimus. Ipsa, expedita
+          aspernatur delectus minus perferendis aliquid ipsam ducimus aut
+          aliquam dolorem nostrum commodi doloremque hic quibusdam itaque
+          molestias maiores. Vitae nesciunt quae saepe debitis. Ad vel quidem
+          ipsum facilis, obcaecati recusandae rerum. Adipisci?
+        </div>
+      </div>
+    </div>
+  );
 };
 export default SinglePostPage;
